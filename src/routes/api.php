300155dbd1ef9f3b4e19f9804ee5af8a587df46d
Yuta <y.sugiyama54@gmail.com>
2020-01-07 23:15:20 +0900

    セミコロンの前に空欄があったため修正

--- src/routes/api.php
+++ src/routes/api.php
@@ -18,7 +18,7 @@ Route::middleware('auth:api')->group(function () {
     Route::post('/contacts', 'ContactsController@store');
     Route::get('/contacts/{contact}', 'ContactsController@show');
     Route::patch('/contacts/{contact}', 'ContactsController@update');
-    Route::delete('/contacts/{contact}', 'ContactsController@destroy')  ;
+    Route::delete('/contacts/{contact}', 'ContactsController@destroy');
 });
 
 

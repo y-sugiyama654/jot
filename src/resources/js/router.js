2bcb3a82e473da2b95a9f7d3773d652b8dc8fc5e
Yuta <y.sugiyama54@gmail.com>
2020-01-18 20:31:38 +0900

    Refactoring to a ContactsList Component & Birthday View

--- src/resources/js/router.js
+++ src/resources/js/router.js
@@ -5,6 +5,7 @@ import ContactsCreate from "./views/ContactsCreate";
 import ContactsShow from "./views/ContactsShow";
 import ContactsEdit from "./views/ContactsEdit";
 import ContactsIndex from "./views/ContactsIndex";
+import BirthdaysIndex from "./views/BirthdaysIndex";
 
 Vue.use(VueRouter);
 
@@ -15,6 +16,8 @@ export default new VueRouter({
         { path: '/contacts/create', component: ContactsCreate },
         { path: '/contacts/:id', component: ContactsShow },
         { path: '/contacts/:id/edit', component: ContactsEdit },
+
+        { path: '/birthdays', component: BirthdaysIndex },
     ],
     mode: 'history',
 });
